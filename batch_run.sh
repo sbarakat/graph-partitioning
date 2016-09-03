37ad0838bf9e388748a5f644e64850f0a3474e93
Sami Barakat <sami@sbarakat.co.uk>
2016-09-03 14:54:10 +0100

    added script to combine metrics

--- batch_run.sh
+++ batch_run.sh
@@ -20,3 +20,5 @@ ipython graph-partitioning-fennel-weights.py -- -i "/home/sami/py-graph/data/one
 
 ipython graph-partitioning-fennel-weights.py -- -i "/home/sami/py-graph/data/oneshot_fennel_weights.txt" -o "/home/sami/py-graph/output" \
         --num-partitions 4 --num-iterations 10 --prediction-model-cut-off 0.35 --one-shot-alpha 0.5 --restream-batches 10
+
+python combine-metrics.py
